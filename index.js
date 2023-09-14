7cab45a47154bb243b3ff351c1abbc037b22ce1a
Connor Adams <connorvadams@gmail.com>
2023-09-13 18:52:33 -0600

    Defines functions for selecting drivers and calculating fares

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    const firstTwoDrivers = [...arrayOfDrivers].slice(0,2);
+    return firstTwoDrivers;
+};
+
+const returnLastTwoDrivers = arrayOfDrivers => { 
+    const lastTwoDrivers = [...arrayOfDrivers].slice(2,4);
+    return lastTwoDrivers;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => {
+    const fareMultiplier = fare => multiplier * fare;
+    return fareMultiplier;
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, callbackFunction) => callbackFunction(arrayOfDrivers);
+
+
+
